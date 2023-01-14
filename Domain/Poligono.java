5b8ca322a1e495976c98b2c553ddfcea86f6406c
AnToN1o99 <antonio.diluca1999@gmail.com>
2023-01-14 13:10:44 +0100

    Quadrato

---
+++ Domain/Poligono.java
@@ -0,0 +1,12 @@
+package Domain;
+
+public abstract class Poligono {
+    protected int numero_lati;
+
+    public Poligono() {
+    }
+
+    public int getNumero_lati() {
+        return numero_lati;
+    }
+}
---
+++ Domain/Quadrato.java
@@ -0,0 +1,9 @@
+package Domain;
+
+public class Quadrato extends Poligono{
+
+    public Quadrato() {
+        super();
+        super.numero_lati = 4;
+    }
+}
